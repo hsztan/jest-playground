418c58eb97c8677a338de5df1695431229ef257a
Henry Nawrocki <hdev@sztanski.com>
2022-03-29 11:16:53 -0500

    Add error functionality and tests to string-length

--- tests/string-length.test.js
+++ tests/string-length.test.js
@@ -2,12 +2,21 @@ const stringLength = require('../src/string-length');
 
 describe('stringLength', () => {
   it('returns a string character count', () => {
-    //arrange
     const string = 'hello';
     const expected = 5;
-    //act
     const charCount = stringLength(string);
-    //assert
     expect(charCount).toEqual(expected);
+  });
+  test('throw and error if string < 1 char', () => {
+    const string = '';
+    expect(() => {
+      stringLength(string);
+    }).toThrow(/less than 1/);
+  });
+  test('throw and error if string > 10 chars', () => {
+    const string = 'My name is John Smith';
+    expect(() => {
+      stringLength(string);
+    }).toThrow(/has more than 10/);
   });
 });
