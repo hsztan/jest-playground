7e083dad6233f45f89863d80f57e68330574063f
Henry Nawrocki <hdev@sztanski.com>
2022-03-29 12:28:17 -0500

    Add subtract method to calculator and tests

--- src/calculator.js
+++ src/calculator.js
@@ -13,6 +13,19 @@ class Calculator {
     }
     return this.result;
   }
+
+  subtract(mainNum, ...nums) {
+    if (nums.length) {
+      if (!nums.every((num) => typeof num === 'number')) {
+        throw new Error('All arguments must be numbers');
+      }
+      this.result = nums.reduce((acc, curr) => {
+        acc -= curr;
+        return acc;
+      }, mainNum);
+    }
+    return this.result;
+  }
 }
 
 module.exports = Calculator;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -21,4 +21,39 @@ describe('Calculator', () => {
       }).toThrow(/must be numbers/);
     });
   });
+  describe('.subtract', () => {
+    it('returns the sum of numbers given as arguments', () => {
+      const number = 8;
+      const numbersToSubtract = [3, 1];
+      const expectedResult = 4;
+      const result = new Calculator().subtract(number, ...numbersToSubtract);
+      expect(result).toEqual(expectedResult);
+    });
+    it('throws an error if any argument is not a number', () => {
+      const number = 8;
+      const numbersToSubtract = [1, 3, 4, undefined];
+      expect(() => {
+        new Calculator().subtract(number, ...numbersToSubtract);
+      }).toThrow(/must be numbers/);
+    });
+    it('returns a negative number if subtraction results negative', () => {
+      const number = 8;
+      const numbersToSubtract = [10];
+      const expected = -2;
+      const result = new Calculator().subtract(number, ...numbersToSubtract);
+      expect(result).toBe(expected);
+    });
+    it('subtracts decimal numbers', () => {
+      const number = 1.1;
+      const numbersToSubtract = [0.2, 0.3];
+      const expected = 0.6;
+      const result = new Calculator().subtract(number, ...numbersToSubtract);
+      expect(result).toBeCloseTo(expected);
+    });
+  });
+  describe('.multiply', () => {
+    describe('it returns the result of multiplying the arguments', () => {
+      const numbersToMultiply = [];
+    });
+  });
 });
